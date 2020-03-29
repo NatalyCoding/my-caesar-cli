b07ff5506bafe96f99f798bacba9475f6f66ab55
Nataly Panakhyd <natalycoding@gmail.com>
2020-03-29 22:39:10 +0300

    feat: add encription function

---
+++ my-caesar-cli/utils/fileEncription.js
@@ -0,0 +1,36 @@
+const encryption = (key, str, action) => {
+    const allLettersOrder = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const upperCaseLettersStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseLettersStr = 'abcdefghijklmnopqrstuvwxyz';
+    const newLetterArr = [];
+    const alphabetLength = 26;
+    const firstElemIndex = alphabetLength - key;
+
+    function reorderLettersInStr(arr) {
+        const lettersArr = arr.split('');
+        for (let i = firstElemIndex; i < lettersArr.length; i++) {
+            newLetterArr.push(lettersArr[i])
+        }
+        for (let i = 0; i < firstElemIndex; i++) {
+            newLetterArr.push(lettersArr[i])
+        }
+    }
+
+    reorderLettersInStr(upperCaseLettersStr)
+    reorderLettersInStr(lowerCaseLettersStr)
+
+    const newLetterOrder = newLetterArr.join('');
+
+    if (action === 'encode') {
+        const index = x => newLetterOrder.indexOf(x);
+        const translate = x => index(x) > -1 ? allLettersOrder[index(x)] : x;
+        return str.split('').map(translate).join('');
+    }
+    if (action === 'decode') {
+        const index = x => allLettersOrder.indexOf(x);
+        const translate = x => index(x) > -1 ? newLetterOrder[index(x)] : x;
+        return str.split('').map(translate).join('');
+    }
+}
+
+module.exports = { encryption };
